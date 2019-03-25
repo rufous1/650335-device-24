9a5aca4bfae3175ad0afb004bc54ba1e3a37c7f8
rufous1 <46838249+rufous1@users.noreply.github.com>
2019-03-26 00:12:10 +0300

    доработки по проекту

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ var writeUs = document.querySelector(".write-us-link");
 var modalWriteUs = document.querySelector(".modal-write-us");
 var close = modalWriteUs.querySelector(".modal-close");
 var form = modalWriteUs.querySelector(".write-us-form");
-var userName = modalWriteUs.querySelector("[name=user-name]");
-var userEmail = modalWriteUs.querySelector("[name=user-email]");
-var userMessage = modalWriteUs.querySelector("[name=user-message]");
+var userName = modalWriteUs.querySelector(".fieldback-user-name");
+var userEmail = modalWriteUs.querySelector(".fieldback-user-email");
+var userMessage = modalWriteUs.querySelector(".fieldback-user-message");
 
 var isStorageSupport = true;
-var storageName = '';
-var storageEmail = '';
+var storageName = "";
+var storageEmail = "";
 
 try {
-  storageName = localStorage.getItem("name");
-  storageEmail = localStorage.getItem("email");
+  storageName = localStorage.getItem("userName");
+  storageEmail = localStorage.getItem("userEmail");
 } catch (err) {
   isStorageSupport = false;
 }
@@ -20,45 +20,66 @@ try {
 writeUs.addEventListener("click", function(evt) {
   evt.preventDefault();
   modalWriteUs.classList.add("modal-show");
-
-  if (storageName) {
-  	userName.value = storageName;
-  	userEmail.focus();
-  } else {
-  	userName.focus();
-  }
   if (storageName && storageEmail) {
-  	userName.value = storageName;
-  	userEmail.value = storageEmail;
-  	userMessage.focus();
+    userName.value = storageName;
+    userEmail.value = storageEmail;
+    userMessage.focus();
+  } else {
+    userName.focus();
   }
 });
 
 close.addEventListener("click", function(evt) {
   evt.preventDefault();
   modalWriteUs.classList.remove("modal-show");
+  modalWriteUs.classList.remove("modal-error");
 });
 
-form.addEventListener("submite", function(evt) {
+form.addEventListener("submit", function(evt) {
   if (!userName.value || !userEmail.value || !userMessage.value) {
     evt.preventDefault();
-    if (!userName.value) {
-      userName.classList.add("modal-invalid");
-    }
-    if (!userEmail.value) {
-      userEmail.classList.add("modal-invalid");
-    }
+    modalWriteUs.classList.remove("modal-error");
+    modalWriteUs.offsetWidth = modalWriteUs.offsetWidth;
+    modalWriteUs.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
-      localStorage.setItem("name", userName.value);
-      localStorage.setItem("email", userEmail.value);
+      localStorage.setItem("userName", userName.value);
+      localStorage.setItem("userEmail", userEmail.value);
+    }
+  }
+});
+
+window.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modalWriteUs.classList.contains("modal-show")) {
+      modalWriteUs.classList.remove("modal-show");
+      modalWriteUs.classList.remove("modal-error");
     }
   }
 });
 
 
+
+var map = document.querySelector(".map");
+var modalMap = document.querySelector(".modal-map");
+var closeMap = modalMap.querySelector(".modal-close");
+
+map.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalMap.classList.add("modal-show");
+});
+
+closeMap.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalMap.classList.remove("modal-show");
+});
+
 window.addEventListener('keydown', function(evt) {
   if (evt.keyCode === 27) {
-    modalWriteUs.classList.remove('modal-show');
+    evt.preventDefault();
+    if (modalMap.classList.contains("modal-show")) {
+      modalMap.classList.remove("modal-show");
+    }
   }
 });
